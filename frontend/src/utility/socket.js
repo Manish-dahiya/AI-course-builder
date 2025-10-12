2132c6b19f62aa6852280ad68726ce7ccbfbbbb3
Manish-dahiya <manishdahiya5720@gmail.com>
2025-10-12 14:40:09 +0530

    changed the backend api url for socket.io

--- frontend/src/utility/socket.js
+++ frontend/src/utility/socket.js
@@ -1,7 +1,8 @@
 // src/socket.js
 import { io } from "socket.io-client";
+import { API_BASE_URL } from "./helper";
 
 // connect once — backend URL
-export const socket = io("http://localhost:5000", {
+export const socket = io(`${API_BASE_URL}`, {
   autoConnect: false, // manual connect when user logs in
 });
